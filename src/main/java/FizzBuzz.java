59175b2d23ab3a090b333468cad360914942b7bc
Rabin Kandel <kandelrabin@live.com>
2024-05-09 14:33:24 +0100

    Add code for FizzBuzz

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,5 +1,15 @@
 public class FizzBuzz {
+
     public String getFizzBuzz(int number) {
-        return null;
+        if (number%3==0 && number%5==0){
+            return "FizzBuzz";
+        } else if(number%3 == 0){
+            return "Fizz";
+        } else if (number%5 == 0){
+            return "Buzz";
+        } else {
+            return String.valueOf(number);
+        }
     }
+
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,13 +1,14 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 
 public class FizzBuzzTest {
 
+    // arrange step-1
     FizzBuzz fizzBuzz;
 
+    // arrange step-2
     @BeforeEach
     public void setUp(){
         fizzBuzz = new FizzBuzz();
@@ -15,8 +16,37 @@ public class FizzBuzzTest {
 
     @Test
     public void canReturnFizz(){
+        // act
         String result = fizzBuzz.getFizzBuzz(3);
+        // assert
         assertThat(result).isEqualTo("Fizz");
+    }
+
+    @Test
+    public void canReturnBuzz(){
+        // act
+        String result = fizzBuzz.getFizzBuzz(5);
+        // assert
+        String expected = "Buzz";
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void canReturnFizzBuzz(){
+        // act
+        String result = fizzBuzz.getFizzBuzz(15);
+        // assert
+        String expected = "FizzBuzz";
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void canReturnNumberIfNotFizzBuzz(){
+        // act
+        String result = fizzBuzz.getFizzBuzz(2);
+        // assert
+        String expected = "2";
+        assertThat(result).isEqualTo(expected);
     }
 
 }
